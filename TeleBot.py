4efd51a534466cd582db4f36217f43ce546d21b7
F4x <smart.farhan.2000@gmail.com>
2020-04-12 15:16:14 +0300

    Added bunch of features
    - Arabic start message
    - Country not found mesaage

--- TeleBot.py
+++ TeleBot.py
@@ -1,10 +1,18 @@
 import telegram
 from Covid import Statistics
-from telegram.ext import MessageHandler, Filters, Updater
+from telegram.ext import MessageHandler, CommandHandler, Filters, Updater
 
 updater = Updater(token='1220830296:AAFpxTw3yYE_URG0OZxeQFp7BgjXKWomKDc', use_context=True)
 dispatcher = updater.dispatcher
 cls = Statistics()
+
+def start(update, context):
+    start_msg_ar = "للبدء ارسل علم دولتك وسوف أرد عليك بأحدث المعلومات عن فيروس كورونا."
+    Start_msg = "Covid19 bot created By FAR7AN & Moha369, Send your country flag to get the latest information about Corona virus in your country. \n" + start_msg_ar
+    context.bot.send_message(chat_id=update.effective_chat.id, text=Start_msg)
+
+Start = CommandHandler('start', start)
+dispatcher.add_handler(Start)
 
 def Send(update, context):
     msg = update.message
@@ -16,11 +24,12 @@ def Send(update, context):
         data = country.get("code", "")
         list.append(data)
         msg_caps = msg.text.upper()
+
     if msg_caps in list:
         cases = cls.by_country(msg_caps)['total_cases']
         context.bot.send_message(chat_id=update.effective_chat.id, text=cases)
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='Country not Found')
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Country not supported yet, contact creators to add your country...')
 
 
 Send = MessageHandler(Filters.text, Send)
