90dbe3e60d91a9752834961f93b8870169f5b5c9
F4x <smart.farhan.2000@gmail.com>
2020-04-12 05:08:38 +0300

    sending real data from Covid.py to telegram bot
    Added checking for country existance

--- TeleBot.py
+++ TeleBot.py
@@ -2,19 +2,25 @@ import telegram
 from Covid import Statistics
 from telegram.ext import MessageHandler, Filters, Updater
 
-
-
-
 updater = Updater(token='1220830296:AAFpxTw3yYE_URG0OZxeQFp7BgjXKWomKDc', use_context=True)
-
 dispatcher = updater.dispatcher
+cls = Statistics()
 
 def Send(update, context):
-    print(update.message)
-    if f'{update.message.text}'.lower() == 'hello':
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Hi")
+    msg = update.message
+    print(msg)
+
+    countries = cls.countries_info
+    list = []
+    for country in countries:
+        data = country.get("code", "")
+        list.append(data)
+        msg_caps = msg.text.upper()
+    if msg_caps in list:
+        cases = cls.by_country(msg_caps)['total_cases']
+        context.bot.send_message(chat_id=update.effective_chat.id, text=cases)
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='Error')
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Country not Found')
 
 
 Send = MessageHandler(Filters.text, Send)
